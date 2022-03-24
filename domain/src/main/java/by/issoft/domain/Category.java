c7ce4e2b7cae107aa9f41c1d18f0e6d8c7086e91
Rodion <aparinrodion@gmail.com>
2022-03-24 15:10:53 +0300

    added CategoryTest.java

--- domain/src/main/java/by/issoft/domain/Category.java
+++ domain/src/main/java/by/issoft/domain/Category.java
@@ -3,18 +3,19 @@ package by.issoft.domain;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Category {
-    public String getName() {
-        return name;
-    }
-
     private final String name;
     private final List<Product> productList;
 
     public Category(String name) {
         this.name = name;
         productList = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void addProduct(Product product) {
@@ -24,17 +25,18 @@ public class Category {
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder("Category: " + name + "\nProducts:\n");
-        for (Product product : productList
-        ) {
-            stringBuilder.append(product).append("\n");
-        }
+        productList.forEach(product -> stringBuilder.append(product).append("\n"));
         return stringBuilder.toString();
     }
 
     public String getSortedProducts(Comparator<Product> comparator) {
         StringBuilder stringBuilder = new StringBuilder("Category: " + name + "\nProducts:\n");
-        productList.stream().sorted(comparator).forEach(product -> stringBuilder.append(product).append("\n"));
+        getSortedProduct(comparator).forEach(product -> stringBuilder.append(product).append("\n"));
         return stringBuilder.toString();
+    }
+
+    public List<Product> getSortedProduct(Comparator<Product> comparator) {
+        return productList.stream().sorted(comparator).collect(Collectors.toList());
     }
 
     public List<Product> getProductList() {
---
+++ domain/src/test/java/by/issoft/domain/CategoryTest.java
@@ -0,0 +1,72 @@
+package by.issoft.domain;
+
+import by.issoft.domain.categories.BikeCategory;
+import by.issoft.domain.categories.MilkCategory;
+import by.issoft.domain.categories.PhoneCategory;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    @Test
+    void getName() {
+        Category category = new Category("name");
+        assertEquals("name", category.getName());
+
+        category = new BikeCategory();
+        assertEquals("Bike", category.getName());
+
+        category = new MilkCategory();
+        assertEquals("Milk", category.getName());
+
+        category = new PhoneCategory();
+        assertEquals("Phone", category.getName());
+    }
+
+    @Test
+    void addProduct() {
+        Category category = new Category("name");
+        Product product1 = new Product("product name", 10., 20.);
+        List<Product> productList = new ArrayList<>();
+        productList.add(product1);
+        category.addProduct(product1);
+        assertEquals(productList, category.getProductList());
+
+        Product product2 = new Product("product name", 20., 30.);
+        productList.add(product2);
+        category.addProduct(product2);
+        assertEquals(productList, category.getProductList());
+    }
+
+    @Test
+    void getSortedProducts() {
+        Category category = new Category("category");
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product("a", 5.6, 50.));
+        productList.add(new Product("a1", 5.6, 51.));
+        productList.add(new Product("b", 5.4, 51.));
+        productList.add(new Product("b1", 5.2, 53.));
+        productList.add(new Product("c", 5.2, 52.));
+        productList.add(new Product("c1", 5.1, 55.));
+        productList.forEach(category::addProduct);
+
+        Comparator<Product> comparator = Comparator
+                .comparing(Product::getName, Comparator.reverseOrder())
+                .thenComparing(Product::getPrice)
+                .thenComparing(Product::getPrice, Comparator.reverseOrder());
+        assertEquals(productList.stream().sorted(comparator).collect(Collectors.toList()),
+                category.getSortedProduct(comparator));
+    }
+
+    @Test
+    void getProductList() {
+        Category category = new Category("name");
+        assertEquals(category.getProductList(), new ArrayList<>());
+    }
+}
